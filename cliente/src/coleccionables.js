6d5e29dc1bf8007773a7ba8f3582b2291565a8f8
Matias Rodriguez <matiasezequiel02@gmail.com>
2025-07-09 15:42:32 -0300

    coleccionables cargados desde el back

--- cliente/src/coleccionables.js
+++ cliente/src/coleccionables.js
@@ -2,8 +2,24 @@
 
 const BTN_MOVIES = document.getElementById("movies-button");
 const BTN_CARRITO = document.getElementById("carrito-button");
+const SECTION_PRODUCTOS = document.getElementById("section-productos");
+const url = "http://localhost:3000";
 
 let carrito = JSON.parse(sessionStorage.getItem("carrito")) || [];
+let coleccionables = [];
+
+async function cargar_coleccionables(){
+
+    try{
+
+        let peticion = await fetch(`${url}/collectibles`);
+        let datos = await peticion.json();
+        return datos.payload;
+
+    }catch(error){
+        console.log(error);
+    }
+}
 
 BTN_MOVIES.addEventListener("click", () => {
     location.href = "../main.html";
@@ -21,10 +37,48 @@ function verificar_nombre(){
         location.href = "../index.html";
     }
 
-    NOMBRE_USUARIO.innerHTML = "Hola, " + nombre;
+    //NOMBRE_USUARIO.innerHTML = "Hola, " + nombre;
 
 }
 
-function init(){
+
+function mostrar_coleccionables(){
+
+    let html = "";
+
+    coleccionables.forEach((c) => {
+
+        html += `<div class="container-producto">
+
+                <div class="card-producto">
+    
+                    <div class="prod-img-container"><img class="imagen-prod" src="${c.imagen}" alt="${c.titulo}"></div>
+    
+                    <div class="prod-text">
+    
+                        <h3 class="movie-title">${c.nombre}</h3>
+    
+                        <p>${c.descripcion}</p>
+    
+                        <p>Precio: ${c.precio}$</p>
+    
+                    </div>
+    
+                    <div><button id=boton-${c.id} class="boton-añadir">Añadir</button></div>
+    
+                </div>
+                </div>`;
+   });
+   
+   SECTION_PRODUCTOS.innerHTML = html;
+
+}
+
+
+async function init(){
     verificar_nombre();
+    coleccionables = await cargar_coleccionables();
+    mostrar_coleccionables();
 }
+
+init();
